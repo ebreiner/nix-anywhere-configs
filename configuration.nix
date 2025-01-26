cb77b045b73bb686d42de5abeada9e9f6a7cb889
ebreiner <51745380+ebreiner@users.noreply.github.com>
2025-01-26 14:48:11 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -23,10 +23,10 @@
   nix.settings.substituters = [
     "https://nix-community.cachix.org"
     "https://cache.nixos.org"
-    "https://nix-cache.aermel.net"
   ];
   nix.settings.trusted-public-keys = [
     "nix-cache.aermel.net:nIgjKq1imiwaDAF0YtMsJ84AYVQPODXaXN4P2f3rI58="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
 
   environment.systemPackages = map lib.lowPrio [
